be77b5019171a04359e1913881a769238d277940
Ladrillo <gabriel@cabrejas.net>
2022-08-10 19:34:38 +0200

    committing to "lecture"

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -45,11 +45,12 @@ function useLS(key, value) { // work just like `useState('foobar', [1,2,3])`
   // window.localStorage.setItem('foobar', JSON.stringify([1, 2, 3]))
   const [data, setData] = useState(() => {
     const fromLocalStorage = window.localStorage.getItem(key)
-    debugger
     return fromLocalStorage
       ? JSON.parse(fromLocalStorage)
       : value
   })
+
+  
 
   return [data, Function.prototype]
 }
