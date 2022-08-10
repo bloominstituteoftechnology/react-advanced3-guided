2ad3054119f8c4b08c26c7599ec9b277bf8963d8
Ladrillo <gabriel@cabrejas.net>
2022-08-10 18:12:47 +0200

    committing to "lecture"

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function useRandomQuote() {
-  
+  const [quote, setQuote] = useState({})
 }
 
 function useCount(initial) {
