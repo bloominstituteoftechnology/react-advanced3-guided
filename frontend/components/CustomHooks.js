b76c84fb9527465a7107978a1276e3690a233a2c
Ladrillo <gabriel@cabrejas.net>
2022-08-10 16:35:04 +0200

    initial

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -2,59 +2,36 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function useRandomQuote() {
-  let [quote, setQuote] = useState({})
-  useEffect(() => {
-    axios.get('http://localhost:9000/api/quotes')
-      .then(res => {
-        const length = res.data.quotes.length
-        const randomNum = Math.floor(Math.random(length) * length)
-        const randomQuote = res.data.quotes[randomNum]
-        setQuote(randomQuote)
-      })
-  }, [])
-  return quote
+
 }
 
 function useCount(initial) {
-  const [count, setCount] = useState(initial)
-  const inc = () => setCount(count + 1)
-  const dec = () => setCount(count - 1)
-  return [count, inc, dec]
+
 }
 
-function useForm(key, keys) {
-  const [form, setForm] = useLS(key, keys)
-  const onChange = ({ target: { name, value } }) =>
-    setForm({ ...form, [name]: value })
-  return [form, onChange]
+function useForm(key, values) {
+
 }
 
 function useLS(key, value) {
-  const [valueLS, setValueLS] = useState(() => {
-    const item = window.localStorage.getItem(key)
-    return JSON.parse(item) || value
-  })
-  const setValue = (value) => {
-    const stringifiedValue = JSON.stringify(value)
-    window.localStorage.setItem(key, stringifiedValue)
-    setValueLS(value)
-  }
-  return [valueLS, setValue]
+
 }
 
 export default function App() {
-  const [form, onChange] = useForm('form', { foo: '', bar: '', baz: '' })
-  const [count, inc, dec] = useCount(7)
-  const quote = useRandomQuote()
+
   return (
     <>
       <h2>Custom Hooks</h2>
-      <p>{quote.text}</p>
-      <p>{count} <button onClick={inc}>inc</button><button onClick={dec}>dec</button></p>
+
+      <p>The more clearly we can focus our attention on the wonders and realities
+        of the universe about us, the less taste we shall have for destruction.</p>
+
+      <p>7 <button>inc</button><button>dec</button></p>
+
       <form>
-        <input onChange={onChange} value={form.foo} name="foo" placeholder="type foo" /><br />
-        <input onChange={onChange} value={form.bar} name="bar" placeholder="type bar" /><br />
-        <input onChange={onChange} value={form.baz} name="baz" placeholder="type baz" />
+        <input name="foo" placeholder="type foo" /><br />
+        <input name="bar" placeholder="type bar" /><br />
+        <input name="baz" placeholder="type baz" />
       </form>
     </>
   )
