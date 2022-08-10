d02d2cac98e7efd1876784842923a4306d810634
Ladrillo <gabriel@cabrejas.net>
2022-08-10 18:20:04 +0200

    committing to "lecture"

--- frontend/components/CustomHooks.js
+++ frontend/components/CustomHooks.js
@@ -7,7 +7,8 @@ function useRandomQuote() {
     axios.get('http://localhost:9000/api/quotes')
       .then(res => {
         // how do we get the number of quotes?
-        const numOfQuotes = res.data.quotes
+        const numOfQuotes = res.data.quotes.length
+        debugger
       })
       .catch(err => {
         console.log(err.message)
